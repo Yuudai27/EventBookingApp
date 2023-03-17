c7f62085c170ff371cbf9882822673253e55c45c
Andreas <Yuudai27@users.noreply.github.com>
2023-03-18 01:35:24 +0900

    Update setupProxy.js

--- setupProxy.js
+++ setupProxy.js
@@ -1,1 +1,13 @@
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
+/**
+The setupProxy component defines the our node server for the react application.
+**/
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    })
+  );
+};
