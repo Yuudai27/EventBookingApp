6e6481c41c0dad824ff06aedb2f25a54550ce293
Andreas <Yuudai27@users.noreply.github.com>
2023-03-18 01:38:24 +0900

    Create events.sql

---
+++ events.sql
@@ -0,0 +1,20 @@
+USE eventapp;
+
+INSERT INTO `events`(event_name, organizer, event_venue, venue_street_name, venue_house_number, venue_city, venue_country, event_genre, event_date, number_of_seats, ticket_price, event_description)
+VALUES ('Rock n Metal', 'Heavy Hits', 'Metal bowl','endless street','17f','Munich','Germany','music','2023-4-05','260',55.60, 'Greatest line-up of the year!'),
+		 ('Classical Rock', 'Heavy Hits', 'Metal bowl','endless street','17f','Munich','Germany','music','2023-3-20','260',75.80, 'For the good old times sake!'),
+		 ('HipHop-World', 'Rap- Tunes', 'Club TZ','endless street','1','Leipzig','Germany','music','2023-5-01','300',110.60, 'Fresh sounds for everyone.'),
+		 ('Break n Dance', 'Rap- Tunes', 'Ace Club','endless street','3d','Frankfurt','Germany','sports','2023-7-09','400',35.00, 'Lets get moving.'),
+		 ('Boygroup Z', 'POPcult', 'Venue Stadium','endless street','7b','Leipzig','Germany','music','2023-6-03','600',95.30, 'Lets sing together.'),
+		 ('Tennis tournament', 'Tennis-Group', 'Tennis Court Munich','endless street','29i','Munich','Germany','sports','2023-4-02','1000',80.20, 'The tennis event of the year!'),
+		 ('Soccer finals', 'SportsXXL', 'TZ Arena','endless street','48a','Dresden','Germany','sports','2023-8-17','2500',210.95, 'The soccer event of the year!'),
+		 ('Basketball World Cup', 'SportsXXL', 'Sports Arena Berlin','endless street','21','Frankfurt','Germany','sports','2023-6-24','1300',130.65, 'The basket ball event of the year!'),
+		 ('Hockey MashUp', 'SportsXXL', 'Ice cave arena','endless street','15','Hamburg','Germany','sports','2023-10-03','550',44.75, 'The ice hockey event of the year!'),
+		 ('Opera X', 'YourOpera!', 'Theatre house Hamburg','endless street','13','Hamburg','Germany','opera','2023-11-19','350',57.98, 'Opera on a new level.'),
+		 ('The devil inside', 'Theatre n Drama', 'Opera house Chemnitz','endless street','10c','Chemnitz','Germany','theatre','2023-8-30','400',66.80, 'Be suprised!'),
+		 ('Leonardos Pride', 'the Art house', 'the Gallery - Dresden','endless street','8','Dresden','Germany','art','2023-8-25','750',15.95, 'Lets find the beauty.'),
+		 ('Renaissance revived', 'the Art house', 'the Gallery - Dresden','endless street','24h','Dresden','Germany','art','2023-7-16','750',10.25, 'Back in time.'),
+		 ('Colour n Lines', 'the Art house', 'Art place Munich','endless street','20','Munich','Germany','art','2023-3-27','1200',16.75, 'Creativity in its finest form.'),
+		 ('Rockabilly - the Show', 'YourOpera!', 'Theatre house Hamburg','endless street','13','Hamburg','Germany','theatre','2023-5-06','350',68.50, 'Rock with us!');
+		 
+
